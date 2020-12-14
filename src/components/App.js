63d7e60a8d68df3181391c00be16fa7762c786ed
paulmboyce <paul.boyce@goobito.com>
2020-12-14 09:37:16 +0000

    refactor(header): move links to Header component (must be inside router)

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,19 @@
 import React from "react";
-import { MemoryRouter as Router, Route, Link } from "react-router-dom";
+import Header from "../components/Header";
+import { MemoryRouter as Router, Route } from "react-router-dom";
+
 import StreamList from "../components/StreamList";
 import StreamCreate from "../components/StreamCreate";
 import StreamEdit from "../components/StreamEdit";
 import StreamDelete from "../components/StreamDelete";
 import StreamShow from "../components/StreamShow";
 
-
 const App = () => {
 	return (
 		<div>
 			<Router>
 				<div>
-					<button>
-						<Link to="/">List Streams</Link>
-					</button>
-					<button>
-						<Link to="/streams/new">Add a Stream</Link>
-					</button>
-					<button>
-						<Link to="/streams/edit">Edit a Stream</Link>
-					</button>
-					<button>
-						<Link to="/streams/delete">Delete a Stream</Link>
-					</button>
-					<button>
-						<Link to="/streams/show">Show Stream</Link>
-					</button>
-					<hr />
+					<Header />
 					<Route path="/" exact component={StreamList} />
 					<Route path="/streams/new" component={StreamCreate} />
 					<Route path="/streams/edit" component={StreamEdit} />
