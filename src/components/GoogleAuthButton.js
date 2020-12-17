302b330dc4d1f34a602439407d4f80b8d18773d7
paulmboyce <paul.boyce@goobito.com>
2020-12-17 15:29:47 +0000

    refactor(googleauthbutton): remove _gapi useState and just refer to global.gapi

--- src/components/GoogleAuthButton.js
+++ src/components/GoogleAuthButton.js
@@ -5,33 +5,11 @@ import { OAUTH_CLIENT_ID } from "../api/google/oauth";
 
 const GoogleAuthButton = () => {
 	const [_auth2, _setAuth2] = useState(null);
-	const [_gapi, _setGapi] = useState(null);
 	const [isSignedIn, setIsSignedIn] = useState(null);
 
 	useEffect(() => {
-		const setGapi = () => {
-			if (global.gapi) {
-				console.log("Setting _GAPI..");
-				_setGapi(global.gapi);
-			}
-		};
-		const waitForGapi = () => {
-			const id = window.setTimeout(() => {
-				setGapi();
-			}, 500);
-			return id;
-		};
-		if (global.gapi) {
-			setGapi();
-		} else {
-			console.log("Waiting for GAPI...");
-			waitForGapi();
-		}
-	}, []);
-
-	useEffect(() => {
 		const initSigninV2 = function () {
-			_gapi.auth2.init({ clientId: OAUTH_CLIENT_ID }).then((auth2) => {
+			global.gapi.auth2.init({ clientId: OAUTH_CLIENT_ID }).then((auth2) => {
 				console.log("GoogleAuth loaded OK");
 				auth2.isSignedIn.listen(onAuthChange);
 				initStatus(auth2.isSignedIn.get());
@@ -39,12 +17,12 @@ const GoogleAuthButton = () => {
 			});
 		};
 
-		if (_gapi) {
+		if (global.gapi) {
 			console.log("Loading AUTH2..");
-			_gapi.load("client:auth2", initSigninV2);
+			global.gapi.load("client:auth2", initSigninV2);
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [_gapi]);
+	}, []);
 
 	const initStatus = (isSignedIn) => {
 		onAuthChange(isSignedIn);
