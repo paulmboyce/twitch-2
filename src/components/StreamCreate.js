6f1bbde682a3ef4ce25931b0d0ed73ea7de00818
paulmboyce <paul.boyce@goobito.com>
2021-01-05 13:02:21 +0000

    feat(redux-form): add redux-form package, wire into code, & implement a basic form with validation

--- src/components/StreamCreate.js
+++ src/components/StreamCreate.js
@@ -1,11 +1,50 @@
 import React from "react";
+import { reduxForm, Field } from "redux-form";
 
-const StreamCreate = () => {
+const StreamCreate = (props) => {
+	const renderError = ({ touched, error }) => {
+		if (touched && error) {
+			return <span style={{ color: "red" }}>{error}</span>;
+		}
+	};
+
+	const renderInput = ({ input, meta, label }) => {
+		return (
+			<div className="field">
+				<label>{label}</label>
+				<input type="text" {...input} autoComplete="off" />
+				{renderError(meta)}
+			</div>
+		);
+	};
+
+	const onSubmit = (formValues) => {
+		console.log("onSubmit! FORM DATA: ", formValues);
+	};
+
 	return (
 		<div>
 			<h1>StreamCreate</h1>
+			<form className="ui form" onSubmit={props.handleSubmit(onSubmit)}>
+				<Field component={renderInput} name="title" label="title" />
+				<Field component={renderInput} name="desc" label="description" />
+				<button className="ui button blue" type="submit">
+					Save
+				</button>
+			</form>
 		</div>
 	);
 };
 
-export default StreamCreate;
+const validate = (formValues) => {
+	const errors = {};
+	if (!formValues.title) {
+		errors.title = "* Please enter a title";
+	}
+	if (!formValues.desc) {
+		errors.desc = "* Please enter a description";
+	}
+	return errors;
+};
+
+export default reduxForm({ form: "StreamCreate", validate })(StreamCreate);
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { reducer as formReducer } from "redux-form";
 
 import App from "./components/App";
 import reduceSignedInUser from "./redux/reduce/reduceSignedInUser";
 const store = createStore(
 	combineReducers({
 		auth: reduceSignedInUser,
+		form: formReducer,
 	}),
 	composeWithDevTools()
 );
