eb6b34fcb2fa51e9ee31440cb5e958b442601faf
paulmboyce <paul.boyce@goobito.com>
2021-01-06 11:10:53 +0000

    feat(streamlist): get all streams and display in reverse order (latest at top)

--- src/components/StreamList.js
+++ src/components/StreamList.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { restApi } from "../api/axios/axios";
+
+const byIdDesc = (a, b) => b.id - a.id;
 
 const StreamList = () => {
+	const [streams, setStreams] = useState([]);
+	useEffect(() => {
+		restApi.get("/").then(({ data }) => {
+			setStreams(data);
+		});
+	}, []);
+
+	const renderStreams = () => {
+		return streams.sort(byIdDesc).map(({ id, title, desc }) => {
+			return (
+				<div key={id}>
+					<b>{title}</b> - {desc}
+				</div>
+			);
+		});
+	};
+
 	return (
 		<div>
 			<h1>Streams</h1>
+			<hr />
+			{renderStreams()}
 		</div>
 	);
 };
