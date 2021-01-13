e42dceea2d290b9adcac0f741f5542c8698f1adc
paulmboyce <paul.boyce@goobito.com>
2021-01-13 15:38:50 +0000

    refactor(formvalues): alternate way to push state onto our form data befre posting

--- src/redux/compose/createStreamAction.js
+++ src/redux/compose/createStreamAction.js
@@ -1,17 +1,17 @@
 import { CREATE_STREAM } from "../actionTypes";
 import { restApi } from "../../api/axios/axios";
 
-const createStreamAction = (streamValues) => {
+const createStreamAction = (formValues) => {
 	return (dispatch, getState) => {
 		const { auth } = getState();
-		streamValues["owner"] = auth.userId;
-
-		restApi.post("/", streamValues).then(({ data }) => {
-			dispatch({
-				type: CREATE_STREAM,
-				payload: { stream: data },
+		restApi
+			.post("/", { ...formValues, owner: auth.userId })
+			.then(({ data }) => {
+				dispatch({
+					type: CREATE_STREAM,
+					payload: { stream: data },
+				});
 			});
-		});
 	};
 };
 
