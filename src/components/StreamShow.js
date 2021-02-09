cc243743aa6d8c0506d47311d542c583c5c514fa
paulmboyce <paul.boyce@goobito.com>
2021-02-09 14:48:45 +0000

    feat(streamvideo): install flv.js and code to load on show stream, & destroy on unmount
    depends on OBS app configured to stream on "custom" for url "rtmp://localhost/live" and stream key
    of streamId in app.   note that this app is incomplete, because setup of OBS is a manual task for
    each different stream

--- src/components/StreamShow.js
+++ src/components/StreamShow.js
@@ -1,13 +1,36 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import flvjs from "flv.js";
+
 import getStreamAction from "../redux/compose/getStreamAction";
 
 const StreamShow = ({ dispatch, stream, match }) => {
+	const videoRef = React.createRef();
+	const { streamId } = match.params;
+
 	useEffect(function () {
 		if (!stream) {
-			dispatch(getStreamAction(match.params.streamId));
+			dispatch(getStreamAction(streamId));
 		}
 	}, []);
+
+	useEffect(() => {
+		const flvPlayer = flvjs.createPlayer({
+			type: "flv",
+			url: `http://localhost:8000/live/${streamId}.flv`,
+		});
+
+		console.log("REF: ", videoRef.current);
+		if (videoRef.current) {
+			flvPlayer.attachMediaElement(videoRef.current);
+			flvPlayer.load();
+		}
+
+		return () => {
+			console.log("Unmounted...");
+			flvPlayer.destroy();
+		};
+	}, [videoRef]);
 
 	if (!stream) {
 		return <div>Loading...</div>;
@@ -15,8 +38,11 @@ const StreamShow = ({ dispatch, stream, match }) => {
 
 	return (
 		<div>
+			<video ref={videoRef} style={{ width: "100%" }} controls></video>
 			<h1>{stream.title}</h1>
 			<h2>{stream.desc}</h2>
+
+			<video id="videoElement"></video>
 		</div>
 	);
 };
